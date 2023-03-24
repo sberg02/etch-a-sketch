49f33ea8f62ed2ff3c739dfc7606908a42d5e0a1
sberg02 <114252187+sberg02@users.noreply.github.com>
2023-03-24 09:49:37 -0500

    Fixed button functionality. Started styling to resemble etch a sketch board.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+const gridContainer = document.querySelector('#grid-container');
 //For loop to create 16 divs
 for (let i = 0; i < 256; i++) {
 createGrid();
@@ -5,49 +6,50 @@ createGrid();
 
 
 //Listens for hover and changes div to black
-const hoverDiv = document.querySelector('#grid-container');
-hoverDiv.addEventListener('mouseover', (e) => {
+gridContainer.addEventListener('mouseover', (e) => {
     e.target.style.background = 'black';
 });
 
 //Make button run sizeChange function when clicked
-//Not ready to finish this button yet
 const changeButton = document.querySelector('#size-btn');
-changeButton.addEventListener('click', sizeChange)    
+changeButton.addEventListener('click', sizeChange);    
+
+//Make clear function to clear the grid
+const clearButton = document.querySelector('#clear-btn');
+clearButton.addEventListener('click', clearGrid);
 
 //Function to create a div, apply styles, and append to grid container.
 function createGrid() {
-    const gridContainer = document.querySelector('#grid-container');
     const createDiv = document.createElement('div');
     gridContainer.appendChild(createDiv);
     createDiv.classList.add('grid-divs');
 }
 //Function to change grid size based on user input
-//Need to find a way to remove current grid
 function sizeChange () {
     const userInput = prompt('Please enter a grid size up to 100: ');
-    const selectGrid = document.getElementById('grid-container');
-    //For whatever reason this works and removeChild does not
-    selectGrid.innerHTML = '';
-    for (let i = 0; i < (userInput * userInput); i++) {
         if (userInput >= 1 && userInput <= 100) {
+            gridContainer.innerHTML = '';
+            for (let i = 0; i < (userInput * userInput); i++)
             createGrid();
-            selectGrid.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
-            selectGrid.style.gridTemplateRows = `repeat(${userInput}, 1fr)`;
+            gridContainer.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
+            gridContainer.style.gridTemplateRows = `repeat(${userInput}, 1fr)`;
         }
-        //Fix this!
-        else if (!isNaN(userInput)) {
-            alert('Not a number!');
-            return;
-        }
-        else if (userInput < 1 || userInput > 100) {
-            alert('Number must be between 1-100')
-            return;
+        else if (isNaN(userInput)) {
+            alert('That\'s not a number! Please enter an number between 1 and 100.');
+            return
         }
         else {
+            alert('Invalid input! Please enter a number between 1 and 100.');
             return
         }
     }
+
+
+function clearGrid () {
+    const selectDivs = document.getElementsByClassName('grid-divs');
+for (let i = 0; i < selectDivs.length; i++) {
+  selectDivs[i].style.backgroundColor = 'white';
+}
 }
 
 //Add button to refresh page like RPS game. Also fix container dimensions.
